6848148467b91e2a34fc06f2180cde2b832ebf27
Ilja Honkonen <ilja.honkonen@fmi.fi>
2025-05-09 13:13:34 +0300

    More renames.

--- tests/mhd/solar_wind_box.cpp
+++ tests/mhd/solar_wind_box.cpp
@@ -456,7 +456,7 @@ int main(int argc, char* argv[]) {
 			not pamhd::mhd::save(
 				boost::filesystem::canonical(
 					boost::filesystem::path(options_sim.output_directory)
-				).append("mhd_staggered_").generic_string(),
+				).append("mhd_").generic_string(),
 				grid,
 				file_version,
 				simulation_step,
@@ -527,7 +527,7 @@ int main(int argc, char* argv[]) {
 				not pamhd::mhd::save(
 					boost::filesystem::canonical(
 						boost::filesystem::path(options_sim.output_directory)
-					).append("mhd_staggered_").generic_string(),
+					).append("mhd_").generic_string(),
 					grid,
 					file_version,
 					simulation_step,
